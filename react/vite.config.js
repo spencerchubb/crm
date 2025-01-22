d65fd79bc304b0aee61ec8f0dcf96678169204d5
spencerchubb <spencerchubb@gmail.com>
2025-01-21 20:48:49 -0600

    add issue and new_issue to vite config

--- react/vite.config.js
+++ react/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: './index.html',
+        issue: './issue/index.html',
+        new_issue: './new_issue/index.html',
+      },
+    },
+  },
 })
