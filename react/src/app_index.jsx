bef4d128b411081eaf427f8315825a41eb78000b
spencerchubb <spencerchubb@gmail.com>
2025-01-22 11:32:35 -0600

    filter by open or completed

--- react/src/app_index.jsx
+++ react/src/app_index.jsx
@@ -3,28 +3,49 @@ import { supabase } from './supabase';
 import { AuthWrapper, useAuthWrapper } from './AuthWrapper';
 import { Timestamp } from './components/Timestamp';
 
+const SHOW_OPEN_ISSUES_DEFAULT = true;
+
 function App() {
   const authWrapperHook = useAuthWrapper();
   const [issues, setIssues] = useState([]);
+  const [showOpenIssues, setShowOpenIssues] = useState(SHOW_OPEN_ISSUES_DEFAULT);
 
-  async function fetchData(session) {
+  async function fetchIssues(showOpenIssues) {
+    console.log('fetching issues', showOpenIssues);
     // To make it possible to join with users, we created this view:
     // https://supabase.com/dashboard/project/pokkflfmgpbgphcredjk/sql/1ba57cf9-8bea-49c0-a91b-946adab6d8ef
-    const { data: issues, error: issuesError } = await supabase
-      .from('issues')
-      .select(`*, users(raw_user_meta_data), attached_labels(labels(id, name))`)
-      .order('created_at', { ascending: false });
+    let query = supabase.from('issues').select(`*, users(raw_user_meta_data), attached_labels(labels(id, name))`);
+    if (showOpenIssues) {
+      query = query.is('completed_at', null).order('created_at', { ascending: false });
+    } else {
+      query = query.not('completed_at', 'is', null).order('completed_at', { ascending: false });
+    }
+    const { data: issues, error: issuesError } = await query;
     if (issuesError) {
       console.error(issuesError);
       return;
     }
     setIssues(issues);
+  }
+
+  async function fetchData(session) {
+    await fetchIssues(SHOW_OPEN_ISSUES_DEFAULT);
     authWrapperHook.setUser(session?.user);
   }
 
   return <AuthWrapper fetchData={fetchData} authWrapperHook={authWrapperHook}>
     <div style={{ padding: 16, width: '100%', maxWidth: 700 }}>
-      <div style={{ display: 'flex', justifyContent: 'end', gap: 8 }}>
+      <div style={{ display: 'flex', gap: 8 }}>
+        <select value={showOpenIssues} onChange={async e => {
+          // const value = e.target.value;
+          // parse bool
+          const value = parseInt(e.target.value);
+          await fetchIssues(value);
+          setShowOpenIssues(value);
+        }}>
+          <option value={1}>Open</option>
+          <option value={0}>Completed</option>
+        </select>
         <a href="/labels/"><button className="btnSecondary">Labels</button></a>
         <a href="/new_issue/"><button className="btnPrimary">New Issue</button></a>
       </div>
